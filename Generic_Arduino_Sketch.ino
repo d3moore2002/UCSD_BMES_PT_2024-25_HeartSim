d1c54f1abeadc616dca524d5a4f1303f5def520f
Anika <anyogi@ucsd.edu>
2025-01-26 14:01:57 -0800

    Update Generic_Arduino_Sketch.ino

--- Generic_Arduino_Sketch.ino
+++ Generic_Arduino_Sketch.ino
@@ -11,27 +11,30 @@ to pin number 2 if a conditional is met.
 */
 
 int sensorPin = A1; //define pin A1 (analog pin)
-int ledPin = 2; //define pin 2 (digital pin)
+int ledPintwo = 2; //define pin 2 (digital pin)
+int ledPinfour = 4; // defined pin 3
 int sensorValue; //create variable for storing readings
+int sensorPinThree = 3; // defined pin 3
+
 
 //void setup is for configurations on start up
 void setup() { 
     Serial.begin(9600); //initialize serial communication
-    pinMode(ledPin, OUTPUT); //define ledPin as an output
+    pinMode(ledPintwo, INPUT); //define ledPin as an output
+    pinMode(ledPinfour, INPUT); //define ledPin as an output
 }
 
 void loop() {
-    sensorValue = analogRead(sensorPin); // do a sensor reading
-    
-    Serial.print("Sensor value is: "); //print a message to the serial monitor
-    Serial.println(sensorValue); //print the value to the serial monitor
-    
-    //check if sensorValue is below 50
-    if(sensorValue < 50) { 
-        digitalWrite(ledPin, HIGH); //if it is, turn on the LED on pin 2.
+  int button = digitalRead(sensorPinThree); // reads if the button connected to pin 3 is on or off
+    if(button, HIGH){
+      digitalWrite(ledPintwo, HIGH); // turns led of pin 2 on
+      digitalWrite(ledPinfour, LOW); // turns off led of pin 4
+
     }
-    //if sensorValue is above 50, turn off the LED
-    else{ 
-        digitalWrite(ledPin, LOW);
+    else{
+      digitalWrite(ledPinfour, HIGH); // turns led of pin 4 on
+      digitalWrite(ledPintwo, LOW); // turns led of pin 2 off
+
     }
+
 }
