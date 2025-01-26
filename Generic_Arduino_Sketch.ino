673b1bc0bd90d954d9f7985e0b53a3a107c5bb89
Aiden Momtaz <aidenmomtaz@gmail.com>
2025-01-26 00:10:30 -0800

    Update Generic_Arduino_Sketch.ino

--- Generic_Arduino_Sketch.ino
+++ Generic_Arduino_Sketch.ino
@@ -11,17 +11,35 @@ to pin number 2 if a conditional is met.
 */
 
 int sensorPin = A1; //define pin A1 (analog pin)
-int ledPin = 2; //define pin 2 (digital pin)
-int sensorValue; //create variable for storing readings
+int ledPinOn = 2; //define pin 2 (digital pin)
+int buttonPin = 3; //define pin 3 (digital button pin)
+int ledPinOff = 4; //define pin 4 (digital pin)
+int sensorValue; //create variable for storing sensor readings
+int buttonValue; //create variable for storing button readings
 
 //void setup is for configurations on start up
 void setup() { 
     Serial.begin(9600); //initialize serial communication
-    pinMode(ledPin, OUTPUT); //define ledPin as an output
+    pinMode(ledPinOn, OUTPUT); //define ledPinOn as an output
+    pinMode(ledPinOff, OUTPUT); //define ledPinOff as an output
+    pinMode(buttonPin, INPUT); //define buttonPin as an input
 }
 
 void loop() {
     sensorValue = analogRead(sensorPin); // do a sensor reading
+    buttonValue = digitalRead(buttonPin); // do a button reading
+
+    //check if button is off
+    if(buttonValue == 0) {
+        digitalWrite(ledPinOff, HIGH) //if off, turn on pin 4 and turn off pin 2
+        digitalWrite(ledPinOn, LOW)
+
+    }
+    //check if button is on
+    else {
+        digitalWrite(ledPinOn, HIGH) //if on, turn on pin 2 and turn off pin 4
+        digitalWrite(ledPinOff, LOW)
+    }
     
     Serial.print("Sensor value is: "); //print a message to the serial monitor
     Serial.println(sensorValue); //print the value to the serial monitor
