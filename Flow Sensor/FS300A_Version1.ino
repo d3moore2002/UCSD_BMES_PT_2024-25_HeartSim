1a0e200a5414a0091ba18a114bd2bd7a3e0b6683
d3moore2002 <119444555+d3moore2002@users.noreply.github.com>
2025-02-20 15:52:57 -0800

    Create FS300A_Version1.ino

---
+++ Flow Sensor/FS300A_Version1.ino
@@ -0,0 +1,49 @@
+const int sensorPin = 2;
+const int measureInterval = 2500;
+volatile int pulseConter;
+
+// YF-S201
+// const float factorK = 7.5;
+
+// FS300A
+const float factorK = 5.5;
+
+// FS400A
+//const float factorK = 3.5;
+
+void ISRCountPulse()
+{
+  pulseConter++;
+}
+
+float GetFrequency()
+{
+  pulseConter = 0;
+
+  interrupts();
+  delay(measureInterval);
+  noInterrupts();
+
+  return (float)pulseConter * 1000 / measureInterval; 
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(sensorPin), ISRCountPulse, RISING);
+}
+
+void loop()
+{
+  // obtain frequency in Hz
+  float frequency = GetFrequency();
+
+  // calculate flow rate L/min
+  float flow_Lmin = frequency / factorK;
+
+  Serial.print("Frequency: ");
+  Serial.print(frequency, 0);
+  Serial.print(" (Hz)\tFlow rate: ");
+  Serial.print(flow_Lmin, 3);
+  Serial.println(" (L/min)");
+}
