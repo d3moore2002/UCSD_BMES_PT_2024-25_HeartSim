bbd6c71c3685e9304545795346c5de93e6c1e2a3
Aiden Momtaz <aidenmomtaz@gmail.com>
2025-04-27 13:18:44 -0700

    Update Servo_With_Timing.ino

--- myservo/Servo_With_Timing.ino
+++ myservo/Servo_With_Timing.ino
@@ -1,1 +1,33 @@
+#include <Servo.h>
 
+// Adjustable Variable
+int initAngleMitral = 0;    //starting angle for mitral valve
+int initAngleAortic = 180;  //starting angle for aortic valve
+int scanDelay = 30;       //delay (time) between movements
+
+Servo servo;
+int angle = initialAngle;
+
+void setup() {
+  Serial.begin(9600);  // Initialize serial communication
+  servo.attach(9);
+  servo.write(initialAngle);
+}
+
+void loop() { 
+  // Scan from initialAngle to 180 degrees
+  for (angle = initialAngle; angle <= 180; angle++) {                                  
+    servo.write(angle);               
+    Serial.print("Servo Angle: ");
+    Serial.println(angle);  // Print the current angle to Serial Monitor
+    delay(scanDelay);                   
+  } 
+
+  // Now scan back from 180 to initialAngle
+  for (angle = 180; angle >= initialAngle; angle--) {                                
+    servo.write(angle);           
+    Serial.print("Servo Angle: ");
+    Serial.println(angle);  // Print the current angle to Serial Monitor
+    delay(scanDelay);       
+  } 
+}
